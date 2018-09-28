cc7ba5bd43475438d704bbfdbe4df30f59136b68
Zack Frank <frank.zack@gmail.com>
2018-09-28 14:00:40 -0500

    Add logic in providers controller to make first and last name lowercase but capitalize first letter of each

--- app/controllers/v1/providers_controller.rb
+++ app/controllers/v1/providers_controller.rb
@@ -1,12 +1,12 @@
 class V1::ProvidersController < ApplicationController
   def create
     provider = Provider.new(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
+      first_name: params[:first_name].downcase.capitalize,
+      last_name: params[:last_name].downcase.capitalize,
       phone_number: params[:phone_number],
       zip: params[:zip],
       provider_type_id: params[:provider_type_id].to_i,
-      email: params[:email],
+      email: params[:email].downcase,
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
